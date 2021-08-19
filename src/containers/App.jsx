4a9bcac4eb3b6c35cb7b0f5db5082dea2a99daf2
lauchaventura <lautaroventura02@gmail.com>
2021-08-19 14:36:01 -0300

    change color body and header

---
+++ src/containers/App.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import Header from "../components/Header";
+import Search from "../components/Search";
+import Footer from "../components/Footer";
+import Categories from "../components/Categories";
+import Carousel from "../components/Carousel";
+import CarouselItem from "../components/CarouselItem";
+import useInitialState from "../hooks/useInitialState";
+
+import "../assets/styles/App.scss";
+
+const API = "http://localhost:3000/initialState";
+
+const App = () => {
+  const videos = useInitialState(API);
+
+  const renderList = (list = []) => {
+    return (
+      <>
+        {list.map((item) => (
+          <CarouselItem key={item.id} {...item} />
+        ))}
+      </>
+    );
+  };
+
+  return (
+    <div>
+      <Header />
+      <Search />
+      {videos.myList && videos.myList.length > 0 && (
+        <Categories title="Mi Lista">
+          <Carousel>{renderList(videos.myList)}</Carousel>
+        </Categories>
+      )}
+      {videos.trends && videos.trends.length > 0 && (
+        <Categories title="Tendencias">
+          <Carousel>{renderList(videos.trends)}</Carousel>
+        </Categories>
+      )}
+      {videos.originals && videos.originals.length > 0 && (
+        <Categories title="Originales ">
+          <Carousel>{renderList(videos.originals)}</Carousel>
+        </Categories>
+      )}
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
